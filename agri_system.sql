8e619f32bc9bf2b5a46b0952cff8e6aa73ab1d9b
NeilChristianOmana <omananeil9@gmail.com>
2025-10-14 14:40:08 +0800

    Add files via upload

---
+++ agri_system.sql
@@ -0,0 +1,18 @@
+insert into  three_categ(High_Value_Crops, Fishiries, Livestocks)
+value('Anao', 'Anao', 'Anao'),
+('Cagsing', 'Cagsing', 'Cagsing'),
+('Calabawan', 'Calabawan', 'Calabawan'),
+('Cambagte', 'Cambagte', 'Cambagte'),
+('Campisong', 'Campisong', 'Campisong'),
+('Cañorong', 'Cañorong', 'Cañorong'),
+('Guiwanon', 'Guiwanon', 'Guiwanon'),
+('Looc', 'Looc', 'Looc'),
+('Malatbo', 'Malatbo', 'Malatbo'),
+('Mangaco', 'Mangaco', 'Mangaco'),
+('Palanas', 'Palanas', 'Palanas'),
+('Poblacion', 'Poblacion', 'Poblacion'),
+('Salamanca', 'Salamanca', 'Salamanca'),
+('San_Roque', 'San_Roque', 'San_Roque');
+
+ 
+
